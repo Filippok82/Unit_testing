de6ea4c35122df548a6167047e9e4f0ce8b66298
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-12-13 11:03:01 +0300

    Adding first files

---
+++ house_task01/src/Calculator.java
@@ -0,0 +1,58 @@
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+
+    public static double squareRootExtraction(double number) {
+
+        //  0
+        //  Отрицательные числа
+        //  Дробные значения корней
+        //  Целые
+
+        double t;
+        double squareRoot = number / 2;
+        do {
+            t = squareRoot;
+            squareRoot = (t + (number / t)) / 2;
+        }
+        while ((t - squareRoot) != 0);
+        return squareRoot;
+
+        // или просто return Math.sqrt(number);
+    }
+
+    // Нужно написать в калькуляторе метод вычисления суммы покупки со скидкой и проверить его, используя AssertJ
+    // Примерная сигнатура и тело метода:
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        double total_sum;
+        total_sum=purchaseAmount/100*discountAmount;
+        // purchaseAmount - сумма покупки
+        // discountAmount - размер скидки
+        return total_sum; // Метод должен возвращать сумму покупки со скидкой
+    }
+}
---
+++ house_task01/src/seminars/first/Main.java
@@ -0,0 +1,29 @@
+package seminars.first;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+
+// < ---------- Не забудьте добавить библиотеку libs/assertj-core-3.23.1.jar - add as Library...
+
+
+// Задание №1 Нужно написать в калькуляторе метод вычисления суммы покупки (метод принимает сумму, процент
+// скидки и возвращает сумму со скидкой) и проверить его используя AssertJ (отрицательное числа,
+// 0, дроби, проценты >=100%, обычные скидки). Все ошибки должны быть обработаны, при вводе
+// недопустимых аргументов можно выбрасывать (throw new ArithmeticException("Суть ошибки");),
+// но нужно проверить, что все ошибки проверяются в тестах. (Заготовки метода уже есть в классе
+// Calculator - calculatingDiscount)
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    System.out.println();
+
+    }
+}
